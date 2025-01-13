e01f7afc680fc6ab9edb10573bed075cb0392326
Kamil <kamil.karwacki65@gmail.com>
2025-01-13 13:21:52 +0100

    d

--- src/main/java/dk/easv/moviecollection/dal/DAOentities/CategoryDAO.java
+++ src/main/java/dk/easv/moviecollection/dal/DAOentities/CategoryDAO.java
@@ -1,5 +1,25 @@
 package dk.easv.moviecollection.dal.DAOentities;
 
-public class CategoryDAO
+import dk.easv.moviecollection.be.Category;
+import dk.easv.moviecollection.dal.CrudDAO;
+import dk.easv.moviecollection.dal.mappers.CategoryRowMapper;
+
+import java.sql.SQLException;
+
+public class CategoryDAO extends CrudDAO<Category>
 {
+  CategoryRowMapper rowMapper = new CategoryRowMapper();
+
+  public CategoryDAO(){
+    super(Category.class);
+  }
+//  public CategoryDAO(Class<Category> entityClass)
+//  {
+//    super(entityClass);
+//  }
+
+  public Category createReturn(Category newCategory) throws SQLException
+  {
+    return this.insertReturn("insert into category (name) values ?", "return name from category where id = ?", new Object[]{newCategory.getName()}, rowMapper);
+  }
 }
