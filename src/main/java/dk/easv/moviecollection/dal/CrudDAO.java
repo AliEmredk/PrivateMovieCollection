8870cf4c460e5eb96cd6c190c2231cdf2d5972a3
Kamil <kamil.karwacki65@gmail.com>
2025-01-13 11:49:59 +0100

    crud data access objet initialize

---
+++ src/main/java/dk/easv/moviecollection/dal/CrudDAO.java
@@ -0,0 +1,95 @@
+package dk.easv.moviecollection.dal;
+import dk.easv.moviecollection.dal.connection.DBConnection;
+import dk.easv.moviecollection.dal.mappers.IRowMapper;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CrudDAO <T>
+{
+  private final Class<T> entityClass;
+  private final DBConnection db;
+
+  public CrudDAO(Class<T> entityClass)
+  {
+    this.entityClass = entityClass;
+    db = new DBConnection();
+  }
+
+  //Generic method for SELECT
+  protected List<T> select(String query, Object[] params, IRowMapper<T> rowMapper) throws
+      SQLException
+  {
+    try (PreparedStatement stmt = db.getConnection().prepareStatement(query)) {
+      for (int i = 0; i < params.length; i++) {
+        stmt.setObject(i + 1, params[i]);
+      }
+      try (ResultSet rs = stmt.executeQuery()) {
+        List<T> results = new ArrayList<>();
+        while (rs.next()) {
+          results.add(rowMapper.mapRow(rs));
+        }
+        return results;
+      }
+    }
+  }
+
+  protected T insertReturn(String insertQuery, String returnQuery, Object[] params, IRowMapper<T> rowMapper) throws SQLException{
+    Connection connection = db.getConnection();
+    T result = null;
+    connection.setAutoCommit(false);
+    try(PreparedStatement stmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS)){
+      for (int i = 0; i < params.length; i++) {
+        stmt.setObject(i + 1, params[i]);
+      }
+      int affectedRows = stmt.executeUpdate();
+      if(affectedRows == 0){
+        throw new SQLException("No rows affected");
+      }
+      int generatedId;
+
+      try(ResultSet generatedKeys = stmt.getGeneratedKeys()){
+        if(generatedKeys.next()){
+          generatedId = generatedKeys.getInt(1);
+        }else {
+          throw new SQLException("Added but id not returned");
+        }
+      }
+
+      try(PreparedStatement returnStmt = connection.prepareStatement(returnQuery)){
+        returnStmt.setInt(1, generatedId);
+        try(ResultSet returnSet = returnStmt.executeQuery()){
+          if(returnSet.next()){
+            result = rowMapper.mapRow(returnSet);
+          }
+        }
+      }
+      connection.commit();
+    }catch (SQLException e){
+      connection.rollback();
+      throw e;
+    }
+    return result;
+  }
+
+  // Generic method for UPDATE
+  protected void update(String query, Object[] params) throws SQLException {
+    try (PreparedStatement stmt = db.getConnection().prepareStatement(query)) {
+      for (int i = 0; i < params.length; i++) {
+        stmt.setObject(i + 1, params[i]);
+      }
+      stmt.executeUpdate();
+    }
+  }
+
+  // Generic method for DELETE
+  protected void delete(String query, Object[] params) throws SQLException {
+    try (PreparedStatement stmt = db.getConnection().prepareStatement(query)) {
+      for (int i = 0; i < params.length; i++) {
+        stmt.setObject(i + 1, params[i]);
+      }
+      stmt.executeUpdate();
+    }
+  }
+}
