229760a22b0cdb25c7b70d1d0013891bb389b6e3
Kamil <kamil.karwacki65@gmail.com>
2025-01-13 13:28:21 +0100

    row mapper

--- src/main/java/dk/easv/moviecollection/dal/mappers/CategoryRowMapper.java
+++ src/main/java/dk/easv/moviecollection/dal/mappers/CategoryRowMapper.java
@@ -1,5 +1,14 @@
 package dk.easv.moviecollection.dal.mappers;
 
-public class CategoryRowMapper
+import dk.easv.moviecollection.be.Category;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CategoryRowMapper implements IRowMapper<Category>
 {
+  @Override public Category mapRow(ResultSet resultSet) throws SQLException
+  {
+    return null;
+  }
 }
