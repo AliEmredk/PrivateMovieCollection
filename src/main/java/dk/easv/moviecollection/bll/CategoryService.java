819553f30208a322b8165c88fa1e19ab1148db2e
Kamil <kamil.karwacki65@gmail.com>
2025-01-13 13:21:52 +0100

    d

---
+++ src/main/java/dk/easv/moviecollection/bll/CategoryService.java
@@ -0,0 +1,10 @@
+package dk.easv.moviecollection.bll;
+
+import dk.easv.moviecollection.dal.DAOentities.CategoryDAO;
+
+public class CategoryService
+{
+  private final CategoryDAO categoryDAO = new CategoryDAO();
+
+
+}
