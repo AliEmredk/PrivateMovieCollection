6faaef5189973e5e7cad73efce0bce4f0d8f3874
Kamil <kamil.karwacki65@gmail.com>
2025-01-13 13:22:40 +0100

    Revert "d"
    This reverts commit 819553f30208a322b8165c88fa1e19ab1148db2e.

--- src/main/java/dk/easv/moviecollection/dal/DAOentities/CategoryDAO.java
+++ src/main/java/dk/easv/moviecollection/dal/DAOentities/CategoryDAO.java
@@ -1,25 +1,5 @@
 package dk.easv.moviecollection.dal.DAOentities;
 
-import dk.easv.moviecollection.be.Category;
-import dk.easv.moviecollection.dal.CrudDAO;
-import dk.easv.moviecollection.dal.mappers.CategoryRowMapper;
-
-import java.sql.SQLException;
-
-public class CategoryDAO extends CrudDAO<Category>
+public class CategoryDAO
 {
-  CategoryRowMapper rowMapper = new CategoryRowMapper();
-
-  public CategoryDAO(){
-    super(Category.class);
-  }
-//  public CategoryDAO(Class<Category> entityClass)
-//  {
-//    super(entityClass);
-//  }
-
-  public Category createReturn(Category newCategory) throws SQLException
-  {
-    return this.insertReturn("insert into category (name) values ?", "return name from category where id = ?", new Object[]{newCategory.getName()}, rowMapper);
-  }
 }
