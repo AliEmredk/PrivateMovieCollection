b3272b9d7bbcd15b39d5af9041b716d2d89faed1
Kamil <kamil.karwacki65@gmail.com>
2025-01-14 11:42:29 +0100

    Implement dao entities and initialize service to integrate with gui/model

--- src/main/java/dk/easv/moviecollection/dal/DAOentities/CategoryDAO.java
+++ src/main/java/dk/easv/moviecollection/dal/DAOentities/CategoryDAO.java
@@ -1,5 +1,43 @@
 package dk.easv.moviecollection.dal.DAOentities;
 
-public class CategoryDAO
+import dk.easv.moviecollection.be.Category;
+import dk.easv.moviecollection.dal.CrudDAO;
+import dk.easv.moviecollection.dal.mappers.CategoryRowMapper;
+import dk.easv.moviecollection.dal.mappers.IRowMapper;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class CategoryDAO extends CrudDAO<Category>
 {
+  private final IRowMapper<Category> rowMapper;
+
+  public CategoryDAO() {
+    rowMapper = new CategoryRowMapper();
+  }
+
+  public List<Category> getAll() throws SQLException
+  {
+    return this.select("select * from categories", new Object[]{}, rowMapper);
+  }
+
+  public Category createNew(Category category) throws SQLException
+  {
+    return this.insertReturn(
+        "insert into categories (name) values (?)",
+          new Object[]{category.getName()},
+          rowMapper,
+        "select * from categories where id = ?");
+  }
+
+  public void delete(Category category) throws SQLException
+  {
+    this.delete("delete from categories where id = ? ", new Object[]{category.getId()});
+  }
+
+  public void update(Category oldCategory, Category newCategory)
+      throws SQLException
+  {
+    this.update("update categories set name = ? where id = ?", new Object[]{newCategory.getName(), oldCategory.getId()});
+  }
 }
--- src/main/java/dk/easv/moviecollection/dal/mappers/MovieRowMapper.java
+++ src/main/java/dk/easv/moviecollection/dal/mappers/MovieRowMapper.java
@@ -1,5 +1,14 @@
 package dk.easv.moviecollection.dal.mappers;
 
-public class MovieRowMapper
+import dk.easv.moviecollection.be.Movie;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MovieRowMapper implements IRowMapper<Movie>
 {
+  @Override public Movie mapRow(ResultSet resultSet) throws SQLException
+  {
+    return null;
+  }
 }
