9bb110663aa97986d9c27ba79ed85dbbc326a5b4
Pawel <69588920+pawel-easv@users.noreply.github.com>
2025-01-14 21:19:05 +0100

    Added button to go back to categories

--- src/main/java/dk/easv/moviecollection/gui/CategoryController.java
+++ src/main/java/dk/easv/moviecollection/gui/CategoryController.java
@@ -5,11 +5,16 @@ import dk.easv.moviecollection.be.Movie;
 import dk.easv.moviecollection.gui.models.DataModel;
 import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
@@ -19,14 +24,26 @@ public class CategoryController {
     private Category category;
     private final DataModel dataModel = new DataModel();
     private final NodeBuilder nodeBuilder = new NodeBuilder();
-
+    private static final String CATEGORIES_VIEW_PATH = "/dk/easv/moviecollection/categories-view.fxml";
     @FXML
     private Label lblCategoryName;
 
     @FXML
     private FlowPane flowPaneMovies;
 
+    @FXML
+    private void showCategoriesWindow(){
 
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(CATEGORIES_VIEW_PATH));
+        try {
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) flowPaneMovies.getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public void setCategory(Category category) throws SQLException {
         this.category = category;
