a49ac5f30c6d956a64c57c340520bb31bf9c6e04
Emre <aliemreuzunoglu@gmail.com>
2025-01-15 14:58:11 +0100

    added some features to categoryCreator and fixed movieCreator

--- src/main/java/dk/easv/moviecollection/gui/CategoryCreatorController.java
+++ src/main/java/dk/easv/moviecollection/gui/CategoryCreatorController.java
@@ -5,6 +5,7 @@ import dk.easv.moviecollection.bll.CategoryService;
 import dk.easv.moviecollection.gui.models.DataModel;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -65,12 +66,23 @@ public class CategoryCreatorController implements Initializable {
     private void addNewCategory() throws SQLException {
         Category category = new Category();
         category.setName(txtFieldCategoryName.getText());
+        if (category.getName().isEmpty()) {
+            showError("You should enter a valid name");
+        }
         category.setPath(path);
         categoryService.createNew(category);
         Window window = (Stage) txtFieldCategoryName.getScene().getWindow();
         Stage stage = (Stage) window;
         dataModel.loadCategories();
         stage.close();
+    }
+
+    private void showError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
 }
